71bff9c77712d21615c69f3409ed44a32aab7b19
vanvy <vanvy543612@gmail.com>
2019-06-28 13:54:58 +0700

    feat(App): splitting component pages tour guide

--- src/components/user/ListTourGuides.jsx
+++ src/components/user/ListTourGuides.jsx
@@ -7,9 +7,49 @@ import {
   Row,
   Icon
 } from 'antd';
+import { get } from 'lodash';
 
 const { Text } = Typography;
 const { Meta } = Card;
+
+const listData = [
+  {
+    id: '1',
+    name: 'Michael Maximoff',
+    avatar: 'https://www.cg-cooper.com/uploads/7/6/9/1/7691405/published/0c4a1744.jpg?1506475705',
+    img: 'https://lh3.googleusercontent.com/KP0uUZWi2GoW49T17y4P1U1ehscle9qTSivSZc2pDripm2zbtLScw_hR81S6pIyicLSP',
+    liked: '89',
+    post: '14',
+    review: '10'
+  },
+  {
+    id: '2',
+    name: 'Marina Valentine',
+    avatar: 'https://douglassmith.info/wp-content/uploads/DSmithHeadShColor.jpg',
+    img: 'https://cdn.pixabay.com/photo/2016/09/01/10/23/image-1635747_960_720.jpg',
+    liked: '13',
+    post: '4',
+    review: '5'
+  },
+  {
+    id: '3',
+    name: 'Carol Summers',
+    avatar: 'http://madelinemiller.com/wp-content/uploads/2017/11/madeline-miller.jpg',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDWC5q01wnGvA9eS-y5UcqellDLXPB9DFuVJJK1nITwupjqpgy',
+    liked: '25',
+    post: '10',
+    review: '9'
+  },
+  {
+    id: '4',
+    name: 'Elaine Dreifuss',
+    avatar: 'https://dispatch.barnesandnoble.com/content/dam/ccr/bnstores/Contributor/BN_Authors_LP_DavidBaldacci.jpg',
+    img: 'https://p.bigstockphoto.com/jtXew2QDQh6TWr8ha8eT_hp-hero-3.jpg',
+    liked: '25',
+    post: '10',
+    review: '9'
+  }
+];
 
 class ListTourGuides extends React.Component {
   constructor(props) {
@@ -19,42 +59,48 @@ class ListTourGuides extends React.Component {
 
   render() {
     return (
-      <Col lg={6} md={8} sm={12} style={{ marginBottom: '20px' }} xs={24}>
-        <Card
-          actions={[
-            <a href="/profile/:id"><Icon style={{ margin: '10px' }} type="profile" />Trang cá nhân</a>,
-            <span><Icon style={{ margin: '10px' }} type="wechat" />Liên hệ</span>
-          ]}
-          className="card-tour-guide"
-          cover={(
-            <img
-              alt="example"
-              src="https://lh3.googleusercontent.com/KP0uUZWi2GoW49T17y4P1U1ehscle9qTSivSZc2pDripm2zbtLScw_hR81S6pIyicLSP"
-              style={{ height: '100%' }}
-            />
-          )}
-        >
-          <div className="avatar-tourguide">
-            <Avatar
-              alt="Avatar"
-              size={100}
-              src="https://www.cg-cooper.com/uploads/7/6/9/1/7691405/published/0c4a1744.jpg?1506475705"
-            />
-          </div>
-          <Text className="name-tourguide">Michael Maximoff</Text>
-          <Row style={{ textAlign: 'center', marginTop: '15px' }}>
-            <Col span={8}>
-              <Meta description="Yêu thích" title="124" />
-            </Col>
-            <Col span={8}>
-              <Meta description="Bài viết" title="14" />
-            </Col>
-            <Col span={8}>
-              <Meta description="Đánh giá" title="5" />
-            </Col>
-          </Row>
-        </Card>
-      </Col>
+      <div>
+        {listData.map((tourguide, index) => (
+          // eslint-disable-next-line react/jsx-sort-props
+          <Col xs={24} sm={24} md={12} lg={6} style={{ marginBottom: '20px' }}>
+            <Card
+              key={get(tourguide, 'id')}
+              actions={[
+                <a href="/user/:id"><Icon style={{ margin: '10px' }} type="profile" />Cá nhân</a>,
+                <span><Icon style={{ margin: '10px' }} type="wechat" />Liên hệ</span>
+              ]}
+              className="card-tour-guide"
+              cover={(
+                <img
+                  alt="example"
+                  src={get(tourguide, 'img')}
+                  style={{ height: '100%' }}
+                />
+              )}
+            >
+              <div className="avatar-tourguide">
+                <Avatar
+                  alt="Avatar"
+                  size={100}
+                  src={get(tourguide, 'avatar')}
+                />
+              </div>
+              <Text className="name-tourguide">{get(tourguide, 'name')}</Text>
+              <Row style={{ textAlign: 'center', marginTop: '15px' }}>
+                <Col span={8}>
+                  <Meta description="Yêu thích" title={get(tourguide, 'liked')} />
+                </Col>
+                <Col span={8}>
+                  <Meta description="Bài viết" title={get(tourguide, 'post')} />
+                </Col>
+                <Col span={8}>
+                  <Meta description="Đánh giá" title={get(tourguide, 'review')} />
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+        ))}
+      </div>
     );
   }
 }
--- src/containers/TourGuide.jsx
+++ src/containers/TourGuide.jsx
@@ -10,6 +10,7 @@ import {
 import {
   ListTourGuides
 } from 'components/user';
+import { HeadingPage } from 'components/common';
 
 const { Content } = Layout;
 const { Title } = Typography;
@@ -25,35 +26,23 @@ class TourGuide extends React.Component {
     return (
       <Content>
         {/* Header Page */}
-        <Card style={{ margin: '20px 0' }}>
-          <Row gutter={24}>
-            <Col span={12}>
-              <Title level={4} style={{ margin: '5px 0 0' }}>Hướng dẫn viên</Title>
-            </Col>
-            <Col offset={6} span={6}>
-              <Select
-                filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                }
-                optionFilterProp="children"
-                placeholder="Lọc theo ..."
-                showSearch
-                size="large"
-                style={{ width: '100%' }}
-              >
-                <Option value="jack">Được yêu thích nhất</Option>
-                <Option value="lucy">Có nhiều bài viết nhất</Option>
-                <Option value="tom">Có lượt đánh giá</Option>
-              </Select>
-            </Col>
-          </Row>
-        </Card>
+        <HeadingPage title="Hướng dẫn viên">
+          <Select
+            filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+            optionFilterProp="children"
+            placeholder="Lọc theo ..."
+            showSearch
+            size="large"
+            style={{ width: '100%' }}
+          >
+            <Option value="jack">Được yêu thích nhất</Option>
+            <Option value="lucy">Có nhiều bài viết nhất</Option>
+            <Option value="tom">Có lượt đánh giá</Option>
+          </Select>
+        </HeadingPage>
         {/* Danh sách Hướng DV */}
-        <Row gutter={24}>
-          <ListTourGuides />
-          <ListTourGuides />
-          <ListTourGuides />
-          <ListTourGuides />
-          <ListTourGuides />
+        <Row gutter={24} style={{ margin: '20px' }}>
           <ListTourGuides />
         </Row>
       </Content>
